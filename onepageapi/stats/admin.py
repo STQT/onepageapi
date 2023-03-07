d70a989f9ac83689387e3741698e4816f2fcb15a
Kairat Sultan <garri.sultonov@mail.ru>
2023-03-08 04:04:04 +0600

    admin page register

--- onepageapi/stats/admin.py
+++ onepageapi/stats/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from onepageapi.stats.models import TgUser, UserForcing
+
+
+@admin.register(TgUser)
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "tg_id"]
+
+
+@admin.register(UserForcing)
+class UserForcingAdmin(admin.ModelAdmin):
+    list_display = ["tg_user", "created_at",]
